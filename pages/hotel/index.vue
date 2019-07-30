120768e3e78ab16ac97fb1da0ddf7b480cc4b8b0
田振毅 <ttzy0202@gmail.com>
2019-07-30 12:23:43 +0800

    初始化项目

---
+++ pages/hotel/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="index">旅游攻略首页</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.index {
+  font-size: 50px;
+  color: blanchedalmond;
+}
+</style>
